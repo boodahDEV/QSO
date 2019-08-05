4530d888344ac042a1d2bd39c089629fad6be3f1
boodah <boodah21@protonmail.com>
2019-08-05 13:10:05 -0500

    Se hace la instancia a firebase y conexion exitosa.

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -1,10 +1,11 @@
 package main;
 
-import java.io.*;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.*;
+import org.apache.commons.codec.digest.DigestUtils;
 
 public class Principal {
 	protected String user_name_principal;
@@ -12,31 +13,39 @@ public class Principal {
 	protected String base_data_user_global = String.valueOf(Principal.class.getResource("dgbu.dll"));
 	
 	public void valide_access_system() throws Exception {
-		String[] file = base_data_user_global.split("file:/");
-		Stream<String> stream = Files.lines(Paths.get(file[1]));
-		List<String> resultFilter = new ArrayList<>();
+		String[] file = base_data_user_global.split("file:/"); //Le quito el file que me coloca obtener el path con el class get resources
+		Stream<String> stream = Files.lines(Paths.get(file[1])); // se crea una coleccion de String asociados a un archivo, file[1] es el resultado del split donde 1 es el path limpio
+		List<String> resultFilter = new ArrayList<>();// lista de Strinf donde almacenará los datos de coleccion
 		stream.forEach((s)->{ 
 			if (s!=null) {
-				System.out.println("-"+s);	
-					
 					String[] data = s.split(" ");
 					for(int i = 0; i < data.length;i++) {
 						resultFilter.add(data[i]);
-						System.out.println("\t#"+i+" :"+data[i]);
 					}//end for
-					
 			}	
 		});
-        for(int i = 0; i<resultFilter.size();i++) {
-            System.out.println("\t\t#"+i+" "+resultFilter.get(i));
-        }  
+//        for(int i = 0; i<resultFilter.size();i++) {
+//            System.out.println("\t\t#"+i+" "+resultFilter.get(i));
+//        }  
 //		if(user_name_principal.equals(""))
 //			System.out.println(user_name_principal+" Inicio al sistema.");
-	}//fin metodo
+	}//fin metodo de validacion de acceso al sistema
 	
 	
 	public static void main(String args[]) {
 		Principal p = new Principal();
+		
+		
+		try {
+			servidor.Server_connect s= new servidor.Server_connect(); 
+			s.createUser("test@gmail.com", false, "1q2w3e4r5t6y", "boodah", false);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace(); }
+		
+		
+		String textoEncriptadoConSHA=DigestUtils.sha1Hex("Faustino"); 
+		System.out.println("Texto Encriptado con SHA : "+textoEncriptadoConSHA);
 		try {
 			p.valide_access_system();
 		} catch (Exception e) {
---
+++ src/servidor/Server_connect.java
@@ -0,0 +1,270 @@
+package servidor;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.ExportedUserRecord;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import com.google.firebase.auth.ListUsersPage;
+import com.google.firebase.auth.UserRecord;
+import com.google.firebase.auth.UserRecord.CreateRequest;
+import com.google.firebase.auth.UserRecord.UpdateRequest;
+import com.google.firebase.database.*;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Auth snippets for documentation.
+ *
+ * See:
+ * https://firebase.google.com/docs/auth/admin
+ */
+public class Server_connect {
+
+public Server_connect() {
+//	PREPARA LA INSTANCIA A FIREBASE DEPENDENDIENDO DE MIS CREDENCIALES Y ACCESO A MI BASE DE DATOS!
+    try {
+      FileInputStream serviceAccount = new FileInputStream(System.getenv("GOOGLE_APPLICATION_CREDENTIALS"));
+      FirebaseOptions options = new FirebaseOptions.Builder()
+          .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+          .build();
+      FirebaseApp.initializeApp(options);
+
+    } catch (Exception e) {
+      System.out.println("ERROR: invalid service account credentials. See README.");
+      e.printStackTrace();
+
+      System.exit(1);
+    }
+//	PREPARA LA INSTANCIA A FIREBASE DEPENDENDIENDO DE MIS CREDENCIALES Y ACCESO A MI BASE DE DATOS! 
+}
+
+
+
+  public void getUserById(String uid) throws InterruptedException, ExecutionException {
+    // [START get_user_by_id]
+    UserRecord userRecord = FirebaseAuth.getInstance().getUserAsync(uid).get();
+    // See the UserRecord reference doc for the contents of userRecord.
+    System.out.println("Successfully fetched user data: " + userRecord.getUid());
+    // [END get_user_by_id]
+  }
+
+  public void getUserByEmail(String email) throws InterruptedException, ExecutionException {
+    // [START get_user_by_email]
+    UserRecord userRecord = FirebaseAuth.getInstance().getUserByEmailAsync(email).get();
+    // See the UserRecord reference doc for the contents of userRecord.
+    System.out.println("Successfully fetched user data: " + userRecord.getEmail());
+    // [END get_user_by_email]
+  }
+
+  public void getUserByPhoneNumber(
+      String phoneNumber) throws InterruptedException, ExecutionException {
+    // [START get_user_by_phone]
+    UserRecord userRecord = FirebaseAuth.getInstance().getUserByPhoneNumberAsync(phoneNumber).get();
+    // See the UserRecord reference doc for the contents of userRecord.
+    System.out.println("Successfully fetched user data: " + userRecord.getPhoneNumber());
+    // [END get_user_by_phone]
+  }
+
+  public void createUser(String email, boolean emailverified, String pass, String nombre, boolean disabiled ) throws InterruptedException, ExecutionException {
+    // [START create_user]
+    CreateRequest request = new CreateRequest()
+        .setEmail(email)
+        .setEmailVerified(emailverified)
+        .setPassword(pass)
+        .setDisplayName(nombre)
+//        .setPhotoUrl("http://www.example.com/12345678/photo.png")
+        .setDisabled(disabiled);
+
+    UserRecord userRecord = FirebaseAuth.getInstance().createUserAsync(request).get();
+    System.out.println("Successfully created new user: " + userRecord.getUid());
+    // [END create_user]
+  }
+  
+  public void createUserWithUid() throws InterruptedException, ExecutionException {
+	    // [START create_user_with_uid]
+	    CreateRequest request = new CreateRequest()
+	        .setUid("some-uid")
+	        .setEmail("user@example.com")
+	        .setPhoneNumber("+11234567890");
+
+	    UserRecord userRecord = FirebaseAuth.getInstance().createUserAsync(request).get();
+	    System.out.println("Successfully created new user: " + userRecord.getUid());
+	    // [END create_user_with_uid]
+	  }
+  
+  public void updateUser(String uid) throws InterruptedException, ExecutionException {
+	    // [START update_user]
+	    UpdateRequest request = new UpdateRequest(uid)
+	        .setEmail("user@example.com")
+	        .setPhoneNumber("+11234567890")
+	        .setEmailVerified(true)
+	        .setPassword("newPassword")
+	        .setDisplayName("Jane Doe")
+	        .setPhotoUrl("http://www.example.com/12345678/photo.png")
+	        .setDisabled(true);
+
+	    UserRecord userRecord = FirebaseAuth.getInstance().updateUserAsync(request).get();
+	    System.out.println("Successfully updated user: " + userRecord.getUid());
+	    // [END update_user]
+	  }
+
+	  public void setCustomUserClaims(String uid) throws InterruptedException, ExecutionException {
+	    // [START set_custom_user_claims]
+	    // Set admin privilege on the user corresponding to uid.
+	    Map<String, Object> claims = new HashMap<>();
+	    claims.put("admin", true);
+	    FirebaseAuth.getInstance().setCustomUserClaimsAsync(uid, claims).get();
+	    // The new custom claims will propagate to the user's ID token the
+	    // next time a new one is issued.
+	    // [END set_custom_user_claims]
+
+	    String idToken = "id_token";
+	    // [START verify_custom_claims]
+	    // Verify the ID token first.
+	    FirebaseToken decoded = FirebaseAuth.getInstance().verifyIdTokenAsync(idToken).get();
+	    if (Boolean.TRUE.equals(decoded.getClaims().get("admin"))) {
+	      // Allow access to requested admin resource.
+	    }
+	    // [END verify_custom_claims]
+
+	    // [START read_custom_user_claims]
+	    // Lookup the user associated with the specified uid.
+	    UserRecord user = FirebaseAuth.getInstance().getUserAsync(uid).get();
+	    System.out.println(user.getCustomClaims().get("admin"));
+	    // [END read_custom_user_claims]
+	  }
+
+	  public void setCustomUserClaimsScript() throws InterruptedException, ExecutionException {
+	    // [START set_custom_user_claims_script]
+	    UserRecord user = FirebaseAuth.getInstance()
+	        .getUserByEmailAsync("user@admin.example.com").get();
+	    // Confirm user is verified.
+	    if (user.isEmailVerified()) {
+	      Map<String, Object> claims = new HashMap<>();
+	      claims.put("admin", true);
+	      FirebaseAuth.getInstance().setCustomUserClaimsAsync(user.getUid(), claims).get();
+	    }
+	    // [END set_custom_user_claims_script]
+	  }
+
+	  public void setCustomUserClaimsInc() throws InterruptedException, ExecutionException {
+	    // [START set_custom_user_claims_incremental]
+	    UserRecord user = FirebaseAuth.getInstance()
+	        .getUserByEmailAsync("user@admin.example.com").get();
+	    // Add incremental custom claim without overwriting the existing claims.
+	    Map<String, Object> currentClaims = user.getCustomClaims();
+	    if (Boolean.TRUE.equals(currentClaims.get("admin"))) {
+	      // Add level.
+	      currentClaims.put("level", 10);
+	      // Add custom claims for additional privileges.
+	      FirebaseAuth.getInstance().setCustomUserClaimsAsync(user.getUid(), currentClaims).get();
+	    }
+	    // [END set_custom_user_claims_incremental]
+	  }
+
+	  public void listAllUsers() throws InterruptedException, ExecutionException  {
+	    // [START list_all_users]
+	    // Start listing users from the beginning, 1000 at a time.
+	    ListUsersPage page = FirebaseAuth.getInstance().listUsersAsync(null).get();
+	    while (page != null) {
+	      for (ExportedUserRecord user : page.getValues()) {
+	        System.out.println("User: " + user.getUid());
+	      }
+	      page = page.getNextPage();
+	    }
+
+	    // Iterate through all users. This will still retrieve users in batches,
+	    // buffering no more than 1000 users in memory at a time.
+//	    page = FirebaseAuth.getInstance().listUsersAsync(null).get();
+//	    for (ExportedUserRecord user : page.iterateAll()) {
+//	      System.out.println("User: " + user.getUid());
+//	    }
+	    // [END list_all_users]
+	  }
+
+	  public void deleteUser(String uid) throws InterruptedException, ExecutionException {
+	    // [START delete_user]
+	    FirebaseAuth.getInstance().deleteUserAsync(uid).get();
+	    System.out.println("Successfully deleted user.");
+	    // [END delete_user]
+	  }
+
+	  public void createCustomToken() throws InterruptedException, ExecutionException {
+	    // [START custom_token]
+	    String uid = "some-uid";
+
+	    String customToken = FirebaseAuth.getInstance().createCustomTokenAsync(uid).get();
+	    // Send token back to client
+	    // [END custom_token]
+	    System.out.println("Created custom token: " + customToken);
+	  }
+
+	  public void createCustomTokenWithClaims() throws InterruptedException, ExecutionException {
+	    // [START custom_token_with_claims]
+	    String uid = "some-uid";
+	    Map<String, Object> additionalClaims = new HashMap<String, Object>();
+	    additionalClaims.put("premiumAccount", true);
+
+	    String customToken = FirebaseAuth.getInstance()
+	        .createCustomTokenAsync(uid, additionalClaims).get();
+	    // Send token back to client
+	    // [END custom_token_with_claims]
+	    System.out.println("Created custom token: " + customToken);
+	  }
+
+	  public void verifyIdToken(String idToken) throws InterruptedException, ExecutionException {
+	    // [START verify_id_token]
+	    // idToken comes from the client app (shown above)
+	    FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdTokenAsync(idToken).get();
+	    String uid = decodedToken.getUid();
+	    // [END verify_id_token]
+	    System.out.println("Decoded ID token from user: " + uid);
+	  }
+
+	  public void verifyIdTokenCheckRevoked(String idToken) throws InterruptedException, ExecutionException {
+	    // [START verify_id_token_check_revoked]
+	    try {
+	      // Verify the ID token while checking if the token is revoked by passing checkRevoked
+	      // as true.
+	      boolean checkRevoked = true;
+	      FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdTokenAsync(idToken, checkRevoked).get();
+	      // Token is valid and not revoked.
+	      String uid = decodedToken.getUid();
+	    } catch (ExecutionException e) {
+	      if (e.getCause() instanceof FirebaseAuthException) {
+	        FirebaseAuthException authError = (FirebaseAuthException) e.getCause();
+	        if (authError.getErrorCode().equals("id-token-revoked")) {
+	          // Token has been revoked. Inform the user to reauthenticate or signOut() the user.
+	        } else {
+	          // Token is invalid.
+	        }
+	      }
+	    }
+	    // [END verify_id_token_check_revoked]
+	  }
+
+	  public void revokeIdTokens(String idToken) throws InterruptedException, ExecutionException { 
+	    String uid="someUid";
+	    // [START revoke_tokens]
+	    FirebaseAuth.getInstance().revokeRefreshTokensAsync(uid).get();
+	    UserRecord user = FirebaseAuth.getInstance().getUserAsync(uid).get();
+	    // Convert to seconds as the auth_time in the token claims is in seconds too. 
+	    long revocationSecond = user.getTokensValidAfterTimestamp() / 1000;
+	    System.out.println("Tokens revoked at: " + revocationSecond);
+	    // [END revoke_tokens]
+
+	    // [START save_revocation_in_db]
+	    DatabaseReference ref = FirebaseDatabase.getInstance().getReference("metadata/" + uid);
+	    Map<String, Object> userData = new HashMap<>();
+	    userData.put("revokeTime", revocationSecond);
+	    ref.setValueAsync(userData).get();
+	    // [END save_revocation_in_db]
+	    
+	  }
+
+}//FIN DE LA CLASE! 
