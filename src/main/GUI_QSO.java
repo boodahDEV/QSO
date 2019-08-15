845e596474be330e3d74f5db7c1c2ced73322de7
boodah <boodah21@protonmail.com>
2019-08-15 10:09:08 -0500

    Los paneles se ajustan, deteccion masiba de objetosV0.3.

--- src/main/GUI_QSO.java
+++ src/main/GUI_QSO.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.lang.reflect.Field;
 import java.awt.Color;
-import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
 
@@ -17,16 +16,17 @@ public class GUI_QSO extends JFrame {
 
 	private JPanel contentPane;
 	protected JButton menu_principal;
-	protected GUI_QSO gui;
+	private JPanel panel;
+
 	/**
 	 * Launch the application.
 	 */
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					GUI_QSO frame = new GUI_QSO();
-					frame.meForme(frame);
 					frame.setLocationRelativeTo(null);
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -39,8 +39,9 @@ public class GUI_QSO extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	
 	public GUI_QSO() {
-	 /*barra inferior*/
+	 
 		setResizable(false);
 		setIconImage(Toolkit.getDefaultToolkit().getImage(GUI_QSO.class.getResource("/image/logo.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -50,38 +51,41 @@ public class GUI_QSO extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		menu_principal = new JButton("");
-		menu_principal.setIconTextGap(-20);
-		menu_principal.setIcon(new ImageIcon(GUI_QSO.class.getResource("/image/menu.png")));
-		menu_principal.setFocusable(false);
-		menu_principal.setContentAreaFilled(false);
-		menu_principal.setBorderPainted(false);
-		menu_principal.setBounds(0, 671, 40, 40);
-		/**/
-		metodo_escuchas_globales(menu_principal);
-		/**/
-		contentPane.add(menu_principal);
+		/*barra inferior*/
 		
+		JPanel jpinferior = new JPanel();
+		jpinferior.setBounds(0, 671, 1018, 40);
+		contentPane.add(jpinferior);
+		jpinferior.setLayout(null);
 		
-	/*barra inferior*/
-	}
-	
-	public void meForme(GUI_QSO gui) {
-		this.gui = gui;
+			menu_principal = new JButton("");
+			menu_principal.setRolloverSelectedIcon(new ImageIcon(GUI_QSO.class.getResource("/image/menu_2.png")));
+			menu_principal.setRolloverIcon(new ImageIcon(GUI_QSO.class.getResource("/image/menu_2.png")));
+			menu_principal.setBounds(0, 0, 40, 40);
+			menu_principal.setIconTextGap(-20);
+			menu_principal.setIcon(new ImageIcon(GUI_QSO.class.getResource("/image/menu.png")));
+			menu_principal.setFocusable(false);
+			menu_principal.setContentAreaFilled(false);
+			menu_principal.setBorderPainted(false);
+			metodo_escuchas_globales(menu_principal);
+			jpinferior.add(menu_principal);
+		
+			/*barra inferior*/
+			
+		JPanel jpspace = new JPanel();
+		jpspace.setBackground(Color.WHITE);
+		jpspace.setBounds(0, 0, 1018, 672);
+		contentPane.add(jpspace);
 	}
 	
 	public void metodo_escuchas_globales(Object objeto_entrante) {
-		if(objeto_entrante instanceof JButton) {
+		if(objeto_entrante instanceof JButton && objeto_entrante.equals(menu_principal)) {
 			((JButton) objeto_entrante).addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					
-			         for(Field f : gui.getClass().getFields()) {
-			          System.out.println("nombre: " + f.getName());
-			         }
-					System.out.println("Es un boton!");
+					//Accion del boton!
 				}
 			});
-		}
+		}//objeto menu_principal
 	}
 }
