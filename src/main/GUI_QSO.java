a71cc11c1397718472d786eee4f21ed1194d6277
boodah <boodah21@protonmail.com>
2019-08-17 09:37:08 -0500

    Control por panel dash correcto.

--- src/main/GUI_QSO.java
+++ src/main/GUI_QSO.java
@@ -4,6 +4,9 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import Animacion.Animacion;
+
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -51,6 +54,16 @@ public class GUI_QSO extends JFrame implements Listener_global_buttons{
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+
+		
+		jpdash = new JPanel();
+		jpdash.setBackground(Color.WHITE);
+//		jpdash.setBounds(-145, 0, 145, 670);//ESTADO NORMALMENTE
+		jpdash.setBounds(0, 0, 145, 670);
+		jpdash.setVisible(false);
+		contentPane.add(jpdash);
+		jpdash.setLayout(null);
+		
 		/*barra inferior*/
 		
 		JPanel jpinferior = new JPanel();
@@ -61,7 +74,7 @@ public class GUI_QSO extends JFrame implements Listener_global_buttons{
 			menu_principal = new JButton("");
 			menu_principal.setRolloverSelectedIcon(new ImageIcon(GUI_QSO.class.getResource("/image/menu_2.png")));
 			menu_principal.setRolloverIcon(new ImageIcon(GUI_QSO.class.getResource("/image/menu_2.png")));
-			menu_principal.setBounds(0, 0, 40, 40);
+			menu_principal.setBounds(2, 2, 30, 35);
 			menu_principal.setIconTextGap(-20);
 			menu_principal.setIcon(new ImageIcon(GUI_QSO.class.getResource("/image/menu.png")));
 			menu_principal.setFocusable(false);
@@ -77,20 +90,26 @@ public class GUI_QSO extends JFrame implements Listener_global_buttons{
 		jpspace.setBounds(0, 0, 1018, 670);
 		contentPane.add(jpspace);
 		jpspace.setLayout(null);
-		
-		JPanel jpdash = new JPanel();
-		jpdash.setBounds(0, 0, 145, 670);
-		contentPane.add(jpdash);
-		jpdash.setLayout(null);
 	}
 
 	@Override
 	public void metodo_escuchas_globales(Object objeto_entrante) {
+		
 			if(objeto_entrante instanceof JButton && objeto_entrante.equals(menu_principal)) {
 				((JButton) objeto_entrante).addActionListener(new ActionListener() {
 					@Override
 					public void actionPerformed(ActionEvent e) {
 						//Accion del boton!
+						if(jpdash.isVisible()) {
+							Animacion.mover_izquierda(0, -145, 1, 1, jpdash);
+							System.out.println("OKx");
+							//No se porque si le quito lo visible (setVisible FALSE) anula el desplazamiento a la izquierda.
+							//try {Thread.sleep(1000);}catch(Exception a) {} 
+						}else {
+							jpdash.setVisible(true);
+							Animacion.mover_derecha(-145, 0, 1, 1, jpdash);
+							System.out.println("OK");
+						}
 					}
 				});
 			}//objeto menu_principal
